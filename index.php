2d66503588e658137f93138c62f40abf016a8f8e
blueleaf254 <riverleaf2002us@yahoo.com>
2016-12-15 12:37:12 -0800

    Added new images & content for f-pages

--- index.php
+++ index.php
@@ -84,14 +84,40 @@ include "inc/nav.php";
 			<div class="container">
 
 				<h1>Accomplishments</h1>
+				
+					<p>You can expect the following skills:</p>
 
 				<div class="content">
 				
-					<h2>HTML & CSS</h2>
+					<div class="wrap-one">				
+				
+						<h2>HTML & CSS</h2>
+						
+							<img src="images/html.jpg" alt="html symbol">
+							
+							<p>
+								Clean, semantically correct HTML and well-organized CSS in order to improve code readability and to easily make changes to the code.
+							</p>
+				
+			
+						<h2>PHPmyadmin</h2>
+						
+							<img src="images/phpmy.png" alt="my PHP Admin symbol">	
+							
+							<p>
+								Competent use of PHPmyadmin for the creation of effective forms and database systems for collection and reporting of information.
+							</p>				
 					
-					<h2>PHP & myPHPadmin</h2>
+						<h2>PHP</h2>               
 
-					<h2>JavaScript & JQuery</h2>					
+							<img src="images/php.png" alt="php symbol">	
+							
+							<p>
+								Use of PHP includes to simplify changes to navigation menus and other items that remain static on multiple pages. This allows for almost effortless future updates.
+							</p>				
+											
+					
+					</div> <!-- .col -->
 
 				</div><!-- .content -->
 
@@ -103,17 +129,38 @@ include "inc/nav.php";
 		<section id="third">
 			<div class="container">
 
-				<h1>Accomplishments</h1>
+				<h1>Accomplishments/Skills Continued</h1>
 
 				<div class="content">
 				
-					<h2>WordPress CMS</h2>
+					<div class="wrap-one">
+				
+						<h2>WordPress CMS</h2>
 
-					<h2>Usability</h2>
-					
-					<h2>Design Fundamentals</h2>					
+							<img src="images/wp.png" alt="word press symbol">
+							
+							<p>
+								Careful creation of WordPress blogs and websites to allow for easier client use and for maintainability.
+							</p>	
+							
+						<h2>JavaScript & JQuery</h2>
+						
+							<img src="images/js.png" alt="javascript symbol">
+							
+							<p>
+								Appropriate usage of jQuery to simplify and speed up development and design aspects of website creation. And, of course, application of JavaScript to add behavioral elements.
+							</p>						
+						
+						<h2>Design Fundamentals</h2>	
 
+							<img src="images/color.jpg" alt="design choices">
+							
+							<p>
+								Basic design abilities to bring together elements for a comprehensive color palette and layout that will not hurt your eyes (or make you vomit). 
+							</p>						
 
+					</div> <!--.wrap-one -->
+						
 				</div><!-- .content -->
 
 		   </div><!-- .container -->
