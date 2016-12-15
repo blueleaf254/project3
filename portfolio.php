76364a4c731c19ba161871d86973c3cb7b05a6a3
blueleaf254 <riverleaf2002us@yahoo.com>
2016-12-14 20:07:51 -0800

    changed portfolio plug in selector classes
    Now the selector classes fit my examples a little better.

--- portfolio.php
+++ portfolio.php
@@ -22,8 +22,8 @@
 $page = 'portfolio';
 include "inc/nav.php"; 
 ?>	
-	
-		<h1>Portfolio Page</h1>
+
+		<h1>Portfolio - Examples of Work</h1>
 		
 			<div class="wrapper" id="contentWrapper">
 				<div class="boundingBox" id="content">
@@ -32,27 +32,25 @@ include "inc/nav.php";
 					<ul id="portfolio-filter">
 						<li><a href="#all" title="">All</a></li>
 						<li><a href="#design" title="" rel="design">Design</a></li>
-						<li><a href="#partner" title="" rel="partner">Agency Partner</a></li>
-						<li><a href="#political" title="" rel="political">Political</a></li>
-						<li><a href="#university" title="" rel="university">University</a></li>
+						<li><a href="#partner" title="" rel="partner">Agency Partner</a></li>						
 						<li><a href="#nonprofit" title="" rel="nonprofit">Non-Profit</a></li>
 						<li><a href="#business" title="" rel="business">Business</a></li>
-						<li><a href="#econdev" title="" rel="econdev">Economic Development</a></li>
+						<li><a href="#cms" title="" rel="cms">CMS</a></li>
 						<li><a href="#jquery" title="" rel="jquery">jQuery</a></li>
 
 					</ul>
 				
 					<ul id="portfolio-list">
-						<li style="display: block;" class="business ecommerce partner cms programming jquery search">
+						<li style="display: block;" class="business design partner jquery">
 							
 							<a href="http://www.urcsc170.org/tanacker/lab09/" title="test"><img src="portfolio_files/swiss.jpg" alt=""></a>
 							
 							<p>
-								An example that uses a Z-pattern.
+								An example that uses a Z-pattern design.
 							</p>
 						</li>
 						
-						<li style="display: block;" class="nonprofit partner cms jquery">
+						<li style="display: block;" class="nonprofit partner cms">
 							
 							<a href="http://www.urcsc174.org/tanacker/lab11/" title=""><img src="portfolio_files/wordpress.jpg" alt=""></a>
 							
@@ -61,37 +59,37 @@ include "inc/nav.php";
 							</p>
 						</li>
 												
-							<li class="business design">
+							<li class="nonprofit">
 								
 								<a href="http://www.urcsc170.org/tanacker/lab01/" title=""><img src="portfolio_files/basic.jpg" alt=""></a>
 								
 								<p>
-									An example of a basic website.
+									An example of a basic informational/brochure website.
 								</p>
 								
 						</li>
 						
-						<li class="nonprofit university design cms jquery video">
+						<li class="nonprofit design">
 						
 							<a href="http://www.urcsc170.org/tanacker/lab06/" title=""><img src="portfolio_files/nyc.jpg" alt=""></a>
 								<p>
-									Example of a government website.
+									Example of a governmental website.
 								</p>
 						</li>	
 
-						<li class="nonprofit university design cms jquery video">
+						<li class="design">
 						
 							<a href="http://www.urcsc170.org/tanacker/lab00/" title=""><img src="portfolio_files/materialize.jpg" alt=""></a>
 								<p>
-									Example a website using Materialize framework.
+									Example of a website using the Materialize framework.
 								</p>
 						</li>
 
-						<li class="nonprofit university design cms jquery video">
+						<li class="design">
 						
 							<a href="http://www.urcsc170.org/tanacker/lab03/" title=""><img src="portfolio_files/bootstrap.jpg" alt=""></a>
 								<p>
-									Example a website using Bootstrap framework.
+									Example a website using the Bootstrap framework.
 								</p>
 						</li>
 						
@@ -99,7 +97,7 @@ include "inc/nav.php";
 		
 				</div>	<!-- .boundingBox -->
 			</div> <!--.wrapper -->	
-			
+		
 <script type="text/javascript">
 
   var _gaq = _gaq || [];
