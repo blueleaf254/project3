3c33a02bf74eed3bee2525d7a1941bd5d5692a56
blueleaf254 <riverleaf2002us@yahoo.com>
2016-12-15 16:31:36 -0800

    restyled contact page
    Still not quite right.

--- contact.php
+++ contact.php
@@ -1,73 +1,88 @@
-<!doctype html>
-
+<!DOCTYPE html>
 <html>
-	<head>
-	
-		<meta charset="utf-8">
-		<title>Contact Page</title>
-		
+
+<head>
+    <meta charset="utf-8">
+    <title>Project Three</title>
+
+		<link rel="stylesheet" href="css/long-scrolly.css">
+		<link rel="stylesheet" href="css/portfolio.css" type="text/css" media="screen">
+		<link href="https://fonts.googleapis.com/css?family=Nunito|Open+Sans" rel="stylesheet">		
+		<script src="js/jquery-1.3.2.min.js" type="text/javascript" charset="utf-8"></script>
+		<script src="js/filterable.pack.js" type="text/javascript" charset="utf-8"></script>
 		<link rel="stylesheet" href="css/styles.css">
-		<link href="https://fonts.googleapis.com/css?family=Nunito|Open+Sans" rel="stylesheet">	
+
 	
-	</head>
-	
-	<body>
-    <!-- Navigation -->
+</head>
+
+<body>
+
+			
+ <!-- Navigation -->
 <?php
 $page = 'contact'; 
 include "inc/nav.php"; 
 ?>			
 	<!-- Contact Form -->
-	
-			<div class="form-style">
-			
-				<h1>Contact Form</h1>
-				
-					<p>
-						Please fill in all of the text fields.
-					</p>
-
-					<form method="post" action="database-write.php" name="myForm" onsubmit="return(validate() & validateEmail() );" >
+	<section id="third">
 		
-						<div>
-							<label>First Name:</label> 
-							<input type="text" name="first_name" id="first_name">
-						</div>
-
-
-						<div>
-							<label>Last Name:</label>
-							<input type="text" name="last_name" id="last_name">
-						</div>
-						
-						<div>
-							<label for="telephone">Telephone:</label>
-							<input type="tel" name="phone" id="phone">
-						</div>
-
-
-						<div>
-							<label>Your Email:</label>
-							<input type="text" name="email" id="email">
-						</div>
-						
-						<div>
-							<br>
-							<label>Your Message and/or Comments:</label>
-							<br>
-							<textarea name ="message" id="message"></textarea>
-						</div>						
-						
-						<input type="submit" value="Send">
+		<div class="container">	
+		
+			<div class="content">
+			
+				<div class="form-style">
 				
-					</form>
+					<h1 class="down-placement">Contact Form</h1>
+					
+						<p>
+							Please fill in all of the text fields.
+						</p>
 
-					<p class="section-wrapper">
-						Style idea borrowed from: <a href="https://www.sanwebe.com/2013/10/css-html-form-styles">https://www.sanwebe.com/2013/10/css-html-form-styles</a>
-					</p>	
+						<form method="post" action="database-write.php" name="myForm" onsubmit="return(validate() & validateEmail() );" >
 			
-			</div> <!-- .form-style -->
-			
+							<div>
+								<label>First Name:</label> 
+								<input type="text" name="first_name" id="first_name">
+							</div>
+
+
+							<div>
+								<label>Last Name:</label>
+								<input type="text" name="last_name" id="last_name">
+							</div>
+							
+							<div>
+								<label>Telephone:</label>
+								<input type="text" name="phone" id="phone">
+							</div>							
+
+							<div>
+								<label>Your Email:</label>
+								<input type="text" name="email" id="email">
+							</div>
+							
+							<div>
+								<br>
+								<label>Your Message and/or Comments:</label>
+								<br>
+								<textarea name ="message" id="message"></textarea>
+							</div>						
+							
+							<input type="submit" value="Send">
+					
+						</form>
+
+						<p class="section-wrapper">
+							Style idea borrowed from: <a href="https://www.sanwebe.com/2013/10/css-html-form-styles">https://www.sanwebe.com/2013/10/css-html-form-styles</a>
+						</p>	
+				
+				</div> <!-- .form-style -->
+					
+			</div><!-- .content -->
+
+		</div><!-- .container -->
+
+	</section>        
 <script type="text/javascript">
    <!--
       // Form validation code will come here.
@@ -128,8 +143,32 @@ include "inc/nav.php";
       }	  
 	  
    //-->
-</script>					
-	</body>
+</script>
 
+
+<!-- Use the following JavaScript code to do the smooth-scroll thing on the internal links -->
+<script src="http://code.jquery.com/jquery-2.2.4.min.js"></script>
+<script>
+// From:    Smooth Scrolling by CHRIS COYIER, updated JANUARY 31, 2016
+//          https://css-tricks.com/snippets/jquery/smooth-scrolling
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 250);
+        return false;
+      }
+    }
+  });
+});
+</script>
+
+
+
+</body>
 
 </html>
